ea4bfeb855a6c7017ebcb182f3144bb8039389ae
vinicius Puente <vinicius.puente@db1.com.br>
2020-02-15 14:55:17 -0300

    add unit test structure

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Login from "@/views/login.view.vue";
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Login, {
       propsData: { msg }
     });
     expect(wrapper.text()).toMatch(msg);
