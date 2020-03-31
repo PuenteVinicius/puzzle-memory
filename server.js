24fca3f60d6ed42b371ab2d0568a3f7012938217
vinicius puente <vinicius@debian.puente>
2020-03-30 22:48:03 -0300

    fix config options

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const serveStatic = require("serve-static");
 
-var app = express();
+const app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
 
 const port = process.env.PORT || 5000;
